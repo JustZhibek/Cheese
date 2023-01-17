3291ff48606ddaa8f234e4b2141cf573ba6b0b6f
justZhibek <kamilovazhibek26@gmail.com>
2023-01-17 15:50:33 +0600

    'hw2'

--- main.py
+++ main.py
@@ -1,50 +1,12 @@
-from aiogram import Bot,Dispatcher,executor,types
-from dotenv import load_dotenv
-from random import choice
-import os
+import logging
+from config import dp
+from aiogram.utils import executor
+from handlers import client, extra, callback
 
-
-load_dotenv()
-bot = Bot(os.getenv('TOKEN'))
-dp = Dispatcher(bot)
-
-@dp.message_handler(commands=['start'])
-async def start_command(message:types.Message):
-    await message.answer(f"Hello,welcome {message.from_user.first_name}")
-    await message.delete()
-
-@dp.message_handler(commands=['help'])
-async def start_command(message:types.Message):
-    await message.answer(f'''Команды:
-     start - начало
-     help - список команд,помощь
-     myinfo - получить информацию о себе
-     picture - показать рандомную картинку''')
-    await message.delete()
-
-
-@dp.message_handler(commands=['myinfo'])
-async def start_command(message:types.Message):
-        await message.answer(text = f'Your ID: {message.from_user.id} \n'
-                                    f'your name: {message.from_user.first_name} \n'
-                                    f'your nickname: {message.from_user.username}')
-
-
-@dp.message_handler(commands=['picture'])
-async def friend_picture(message: types.Message):
-        photo = open('media/'+ choice(os.listdir('media')), 'rb')
-        await bot.send_photo(message.chat.id, photo)
-        await message.delete()
-
-
-@dp.message_handler()
-async def echo(message: types.Message):
-    letters = message.text.split(' ')
-    if len(letters) >= 3:
-        await message.answer(message.text.upper())
-    else:
-        await message.answer(message.text)
-
+client.register_handlers_clients(dp)
+callback.register_handlers_callback(dp)
+extra.register_extra_handlers(dp)
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    logging.basicConfig(level=logging.INFO)
+    executor.start_polling(dp, skip_updates=True)
